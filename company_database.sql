b3d781b13e82eed1508dadbab24d9c5ae12660f9
Navneet Kumar <85820764+mrnavi16@users.noreply.github.com>
2024-04-16 20:48:16 +0530

    Create company_database.sql

---
+++ company_database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE jio;
+USE jio;
+
+CREATE TABLE employee(
+id INT PRIMARY KEY,
+name VARCHAR(100),
+salary INT
+);
+
+INSERT INTO employee
+(id,name,salary)
+VALUES
+(1,"NAVNEET",23000),
+(2,"paritosh",560000);
+
+SELECT * FROM employee
+
+
+
+
+
